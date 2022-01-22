f193e828b726a33bbc00f420dc3c537dde41b963
quazarxx <mysubs666@gmail.com>
2022-01-21 20:35:14 -0500

    removed for new command handler

--- commands/Admin/commands.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'commands',
-    description: 'Displays all commands',
-    execute(client, message, args, user, channel, self) {
-        if (user.badges.moderator || user.badges.broadcaster) {
-            client.say(channel, 'hi admin')
-        } else return;
-    }
-}
